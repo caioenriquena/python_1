ee8d1bb63d468ed2bb381f56a68dfb60a0137e00
Caio Enrique Neves Araújo <Hell23@academicos.com>
2023-10-16 20:44:26 -0300

    agora

---
+++ exer/q1.py
@@ -0,0 +1,13 @@
+from random import randint
+
+#Desenvolva um programa para ler um vetor Q de 20 posições (aceitar somente números positivos). Escrever a seguir:
+# o valor do maior elemento de Q e a respectiva posição que ele ocupa no vetor
+# o valor do menor elemento de Q e a respectiva posição que ele ocupa no vetor
+
+
+Q=[]
+
+for numero in range (20):
+    Q.append(randint(a=1, b=100))
+
+print(Q)    
