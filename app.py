214f9cff7980d90748b062c2291bb57fcd7f1931
Caio Enrique Neves Araújo <Hell23@academicos.com>
2023-11-01 19:22:11 -0300

    hi

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+os.environ['FLASK_DEBUG']='True'
+
+app.debug = os.environ.get('FLASK_DEBUG')== 'True'
 
 @app.route('/')
 def ola():
 
     return render_template('index.html')
-
-app.run()
+if __name__ == "__main__":
+    app.run()
